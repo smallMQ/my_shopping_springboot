fa873a7f51f753780a393997b8ca83aeaa8fb444
MQ <2271443486@qq.com>
2022-10-23 13:07:24 +0800

    添加third模块,注入到微服务,和网关,并测试OssController

--- product/src/test/java/com/smallmq/product/ProductApplicationTests.java
+++ product/src/test/java/com/smallmq/product/ProductApplicationTests.java
@@ -1,8 +1,4 @@
 package com.smallmq.product;
-
-
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.model.PutObjectRequest;
 import com.smallmq.product.entity.BrandEntity;
 import com.smallmq.product.service.BrandService;
 import org.junit.jupiter.api.Test;
@@ -21,15 +17,11 @@ class ProductApplicationTests {
     @Autowired
     private BrandService brandService;
 
-    @Autowired
-    private OSSClient ossClient;
+
 
     @Test
-    void contextLoads() throws FileNotFoundException {
-        // 读取文件
-        InputStream inputStream = new FileInputStream("D:\\desktop\\微信图片_20221005125010.jpg");
-        // 上传文件
-        ossClient.putObject("smallmq", "test.jpg", inputStream);
+    void contextLoads(){
+
     }
 
 }
--- third/src/test/java/com/smallmq/third/ThirdApplicationTests.java
+++ third/src/test/java/com/smallmq/third/ThirdApplicationTests.java
@@ -1,13 +1,25 @@
 package com.smallmq.third;
 
+import com.aliyun.oss.OSSClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class ThirdApplicationTests {
+    @Autowired
+    private OSSClient ossClient;
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws FileNotFoundException {
+        // 读取文件
+        InputStream inputStream = new FileInputStream("D:\\desktop\\微信图片_20221005125010.jpg");
+        // 上传文件
+        ossClient.putObject("smallmq", "haha.jpg", inputStream);
     }
 
 }
